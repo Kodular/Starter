222b2948399d4a51c04f3053e43455184a865c30
Pavitra Golchha <pavitragolchha@gmail.com>
2020-12-29 23:09:46 +0530

    Fix?

--- binary/extractor.v
+++ binary/extractor.v
@@ -9,24 +9,6 @@ fn init_temp_dir() string {
 	return temp_dir
 }
 
-fn extract_adbwinapidll(temp_dir string) {
-	mut arr := []byte{len: adbwinapi_dll_len}
-	unsafe {C.memcpy(arr.data, adbwinapi_dll, adbwinapi_dll_len)}
-	path := os.join_path(temp_dir, 'AdbWinApi.dll')
-	mut f := os.open_file(path, 'wb+') or { panic(err) }
-	f.write(arr)
-	f.close()
-}
-
-fn extract_adbwinusbapidll(temp_dir string) {
-	mut arr := []byte{len: adbwinusbapi_dll_len}
-	unsafe {C.memcpy(arr.data, adbwinusbapi_dll, adbwinusbapi_dll_len)}
-	path := os.join_path(temp_dir, 'AdbWinUsbApi.dll')
-	mut f := os.open_file(path, 'wb+') or { panic(err) }
-	f.write(arr)
-	f.close()
-}
-
 pub fn extract_exe() string {
 	temp_dir := init_temp_dir()
 	$if windows {
---
+++ binary/extractor_windows.v
@@ -0,0 +1,19 @@
+module binary
+
+fn extract_adbwinapidll(temp_dir string) {
+	mut arr := []byte{len: adbwinapi_dll_len}
+	unsafe {C.memcpy(arr.data, adbwinapi_dll, adbwinapi_dll_len)}
+	path := os.join_path(temp_dir, 'AdbWinApi.dll')
+	mut f := os.open_file(path, 'wb+') or { panic(err) }
+	f.write(arr)
+	f.close()
+}
+
+fn extract_adbwinusbapidll(temp_dir string) {
+	mut arr := []byte{len: adbwinusbapi_dll_len}
+	unsafe {C.memcpy(arr.data, adbwinusbapi_dll, adbwinusbapi_dll_len)}
+	path := os.join_path(temp_dir, 'AdbWinUsbApi.dll')
+	mut f := os.open_file(path, 'wb+') or { panic(err) }
+	f.write(arr)
+	f.close()
+}
