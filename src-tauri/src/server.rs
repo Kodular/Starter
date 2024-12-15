450ca3a2c01265eb6a8269825457ab0ead7aad12
Pavitra Golchha <hello@pavi2410.me>
2024-12-15 18:23:48 +0530

    add tauri.localhost to CORS

--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -29,6 +29,7 @@ pub(crate) async fn launch_server() {
                     "https://starter.kodular.io".parse::<HeaderValue>().unwrap(),
                     "https://creator.kodular.io".parse::<HeaderValue>().unwrap(),
                     "https://c.kodular.io".parse::<HeaderValue>().unwrap(),
+                    "http://tauri.localhost".parse::<HeaderValue>().unwrap(),
                     "http://localhost:1420".parse::<HeaderValue>().unwrap(),
                 ])
                 .allow_methods([Method::GET])
